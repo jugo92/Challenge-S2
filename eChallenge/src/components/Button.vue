39cc141f0c3f3b3c20d875aa7703eebbd1061def
Makan Kamissoko <makan.kamissoko@hotmail.fr>
2023-09-24 02:46:37 +0200

    add front with vuejs and tailwindcss init button component

---
+++ eChallenge/src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <a :href="linkAction" :class="color">
+    {{text}}
+  </a>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: ['text', 'color', 'linkAction'],
+}
+</script>
+
+<style scoped>
+
+</style>
