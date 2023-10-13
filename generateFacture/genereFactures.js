b4e5e9eca4850c246d89c17568d5092b87ab5d2e
ady243 <masiviady7@gmail.com>
2023-10-13 12:02:33 +0200

    generate

--- generateFacture/genereFactures.js
+++ generateFacture/genereFactures.js
@@ -11,40 +11,36 @@ function calculateTotal(items) {
     return total.toFixed(2);
 }
 
-
-// const customUserDataDir = path.join(__dirname, 'generateFacture', 'user_data');
-
 async function generateFacture(invoiceData) {
+    const browser = await puppeteer.launch({ executablePath: 'chromium', args: ['--no-sandbox'] });
+
+    console.log(await browser.version());
+
     const template = fs.readFileSync('./generateFacture/template/template.html', 'utf-8');
     const html = ejs.render(template, { invoice: invoiceData, calculateTotal: calculateTotal });
-
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-        ],
-    });
-    console.log(2);
-
-
 
     const page = await browser.newPage();
     await page.setContent(html);
 
+    const pdfPath = path.join(__dirname, `${invoiceData.name}`); 
     await page.pdf({
-        path: `./invoices/invoice_${invoiceData.numero}.pdf`,
+        path: pdfPath,
         format: 'A4',
     });
 
     await browser.close();
+
+    return pdfPath;
 }
 
 const invoiceData = {
+    name: "eChallenge_facture.pdf",
     numero: 1,
-    date: "01/01/2021",
+    date: "01/01/2023",
     client: {
-        nom: "John Doe",
+        nom: "Ady Masivi",
+        titre: "M.",
+        metier: "ingenieur informatique",
         adresse: "1 rue de la Paix",
         codePostal: "75000",
         ville: "Paris",
@@ -62,7 +58,19 @@ const invoiceData = {
             prixUnitaireHT: 200,
             tauxTVA: 20,
         },
+        {
+            designation: "Imprimante",
+            quantite: 1,
+            prixUnitaireHT: 200,
+            tauxTVA: 20,
+        },
     ],
 };
 
-generateFacture(invoiceData);
+generateFacture(invoiceData)
+    .then(pdfPath => {
+        console.log(`PDF generated at ${pdfPath}`);
+    })
+    .catch(err => {
+        console.error(err);
+    });
