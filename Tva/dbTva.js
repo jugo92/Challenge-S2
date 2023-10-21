a6a63266110e80f55444c793c1cf93396de6ff3b
jugo92 <jugzek92@outlook.fr>
2023-10-21 20:21:53 +0200

    update model tva

--- Tva/dbTva.js
+++ Tva/dbTva.js
@@ -1,30 +1,33 @@
 const { Sequelize, Model, DataTypes } = require("sequelize");
 const sequelize = new Sequelize("challenge-s2", "user", "challenge-s2", {
-    host: "mysqldb",
-    dialect: "mysql",
+  host: "localhost",
+  dialect: "mysql",
 });
 
 try {
-    sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+  sequelize.authenticate();
+  console.log("Connection has been established successfully.");
 } catch (error) {
-    console.error('Unable to connect to the database:', error);
+  console.error("Unable to connect to the database:", error);
 }
 
-class Tva extends Model { }
-Tva.init({
+class Tva extends Model {}
+Tva.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      taux: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-      },
-}, {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    taux: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+  },
+  {
     sequelize,
-    modelName: 'tva'
-})
+    modelName: "tva",
+  }
+);
 
 module.exports = Tva;
