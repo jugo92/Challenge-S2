cc713fbe0322df0c084b1db86e62d238b6670fed
ady243 <masiviady7@gmail.com>
2023-10-28 15:31:18 +0200

    must be pushed on develop

--- eChallenge/src/components/Register.vue
+++ eChallenge/src/components/Register.vue
@@ -75,7 +75,7 @@
             </div>
           </div>
           <div class="mb-6 flex items-center">
-          <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded focus:ring-3 focus:ring-blue-300 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required>
+          <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded focus:ring-3 focus:ring-blue-300 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800">
           <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
             J'accepte les <a href="#" class="text-blue-600 hover:underline dark:text-blue-500">conditions générales d'utilisation</a> et la <a href="#" class="text-blue-600 hover:underline dark:text-blue-500">politique de confidentialité</a>.
           </label>
@@ -116,7 +116,7 @@ const user = reactive({
   birthdate: '',
   phone: '',
   address: '',
-  country: 'France',
+  country:'France',
   zip: '',
   city: ''
 });
