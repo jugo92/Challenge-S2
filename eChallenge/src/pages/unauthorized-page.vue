b5ef277f1be50ca4b1e68e993699f02a8d919e03
ady243 <masiviady7@gmail.com>
2023-11-06 18:38:30 +0100

    pages expiration token à configurer

--- eChallenge/src/pages/unauthorized-page.vue
+++ eChallenge/src/pages/unauthorized-page.vue
@@ -6,7 +6,7 @@ import router from '../router'
 onMounted(() => {
   setTimeout(() => {
     router.push('/login');
-  }, 2000);
+  }, 3000);
 });
 
 </script>
@@ -14,7 +14,8 @@ onMounted(() => {
 <template>
     <div class="flex items-center justify-center h-screen">
         <div class="text-center">
-        <h1 class="text-4xl font-bold">Vous n'êtes pas autorisé à accéder à cette page</h1>
+        <h4 class="text-4xl font-bold">Pour des raisons de sécurité, 
+            votre session a expiré. Veuillez vous reconnecter pour continuer à utiliser l'application.</h4>
         <p>Vous allez être redirigé vers la page de connexion dans quelques secondes</p>
         </div>
     </div>
