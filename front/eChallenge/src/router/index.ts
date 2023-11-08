9e7f962e8735809839a6429fd9ae7816896b2059
Makan Kamissoko <makan.kamissoko@hotmail.fr>
2023-11-08 14:29:12 +0100

    add token to verify email replace "<a>" by router-link rename login path

--- front/eChallenge/src/router/index.ts
+++ front/eChallenge/src/router/index.ts
@@ -17,7 +17,7 @@ const router = createRouter({
         },
 
         {
-            path: "/",
+            path: "/login",
             name: "Login",
             component: LoginVue,
         },
